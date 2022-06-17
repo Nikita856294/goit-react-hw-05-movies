0c62cc1c859ef6bafd60cf5cdbb181cb000f703e
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2022-06-18 01:09:37 +0300

    add Container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,22 @@ import Homepage from './Homepage';
 import MoviesPage from './MoviesPage';
 import Navigation from './Navigation';
 import MovieDetailsView from './MovieDetailsView';
+import Container from './Container';
 import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
   return (
     <>
       <ToastContainer />
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="movies" element={<MoviesPage />} />
-        <Route path="movies/:movieId/*" element={<MovieDetailsView />} />
-        <Route path="*" element={<Homepage />} />
-      </Routes>
+      <Container>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId/*" element={<MovieDetailsView />} />
+          <Route path="*" element={<Homepage />} />
+        </Routes>
+      </Container>
     </>
   );
 };
