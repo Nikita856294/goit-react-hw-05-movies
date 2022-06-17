52af173bc15380c4b79fb89445d7ef8286e1ed79
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2022-06-17 20:26:59 +0300

    add Homepage

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { requestTrendingFilms } from '../../services/API';
+import { Link } from 'react-router-dom';
+import { TailSpin } from 'react-loader-spinner';
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+const Homepage = () => {
+  const [films, setFilms] = useState([]);
+  const [status, setStatus] = useState('idle');
+
+  async function trendingFilms() {
+    setStatus('pending');
+    try {
+      const film = await requestTrendingFilms();
+      setStatus('resolved');
+      setFilms(film);
+    } catch {
+      setStatus('rejected');
+    }
+    setStatus('resolved');
+  }
+
+  useEffect(() => {
+    trendingFilms();
+  }, []);
+
+  if (status === 'pending') {
+    <TailSpin height="100" width="100" />;
+  }
+  if (status === 'rejected') {
+    alert('Whoops, something went wrong');
+  }
+  if (status === 'resolved') {
+    return (
+      <>
+        <h3>Trending todays</h3>
+        <ul>
+          {films.map(film => {
+            return (
+              <li key={film.id}>
+                <Link to={`movies/${film.id}`}>{film.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+};
+
+export default Homepage;
